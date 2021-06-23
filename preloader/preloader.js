7e51df3eb3e049c7e70dd6d375609da4b87a1d57
* <mdsaied386@gmail.com>
2021-06-23 22:42:34 +0600

    responsive issue is fixed

--- preloader/preloader.js
+++
@@ -1,9 +0,0 @@
-const  preloader = document.querySelector('.preloader')
-
-window.onload=function(){
-  preloader.classList.add('hide-preloader')
-  preloader.addEventListener('transitionend',function(){
-    preloader.remove()
-  })
-  document.body.style.pointerEvents='all'
-}
